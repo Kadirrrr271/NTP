6c664873a0cdf03eac634d97a26be4afe90817d3
Kadirrrr271 <145561945+Kadirrrr271@users.noreply.github.com>
2025-06-02 20:39:52 +0300

    Add files via upload

---
+++ ConsoleDiziUygulaması/ConsoleDiziUygulaması/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDiziUygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("satır sayısını gir>>>");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Sütun sayısını gir >>>");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("___________");
+            Random rnd = new Random();
+
+            int[,] dizi2 = new int[a, b];
+            for(int i=0; i<a; i++)
+            {
+                for(int j=0;j<b;j++)
+                {
+                    dizi2[i, j] = rnd.Next(0, 50);
+                    Console.Write(dizi2[i, j] + "-");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
