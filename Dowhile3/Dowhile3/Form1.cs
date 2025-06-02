57ff920c506d69e790f7951bebffbcea3d021f72
Kadirrrr271 <145561945+Kadirrrr271@users.noreply.github.com>
2025-06-02 20:42:49 +0300

    Add files via upload

---
+++ Dowhile3/Dowhile3/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dowhile3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            do
+            {
+                i++;
+                if (i % 2 == 0)//Değerin 2 ile bölümünden kalanına bakarı
+            }
+            {
+                continue;
+            }
+            MessageBox.Show("" + i);
+          }  While(in<= 15);
+        }
+    }
+}
